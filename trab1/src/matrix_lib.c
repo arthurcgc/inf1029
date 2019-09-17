9375e053a253fa853f3d8babfcef115da52bbbdf
arthurcgc <arthur.cavalcante.puc@gmail.com>
2019-09-17 18:34:54 -0300

    refactor

--- trab1/src/matrix_lib.c
+++ trab1/src/matrix_lib.c
@@ -1,21 +1,16 @@
 #include "matrix_lib.h"
 
-int scalar_matrix_mult(float scalar_value, Matrix *matrix){
+int scalar_matrix_mult(float scalar_value, Matrix *m){
 	unsigned long int i;
-	unsigned long int h;
-	unsigned long int w;
 
-	h = matrix->height;
-	w = matrix->width;
-
-	if(matrix == NULL || (h*w) % 8 != 0 || h == 0) 
+	if(m == NULL || (m->height*m->width) % 8 != 0 || m->height == 0) 
 		return 0;
 
   	__m256 vec_scalar = _mm256_set1_ps(scalar_value); 
  	
- 	float *buffer = matrix->rows;
+ 	float *buffer = m->rows;
  	
- 	for(i=0; i<h*w; i+=8, buffer+=8){
+ 	for(i=0; i<m->height*m->width; i+=8, buffer+=8){
  		__m256 vec_rows = _mm256_load_ps(buffer);	
  		__m256 vec_result = _mm256_mul_ps(vec_scalar, vec_rows);
  		_mm256_store_ps(buffer, vec_result);
@@ -25,9 +20,8 @@ int scalar_matrix_mult(float scalar_value, Matrix *matrix){
 }
 
 int matrix_matrix_mult(Matrix *mA, Matrix *mB, Matrix *mC){	
-	unsigned long int i,j,k;	
-
-	if(mA == NULL || mB == NULL|| mC == NULL) return 0;
+	if(mA == NULL || mB == NULL|| mC == NULL) 
+		return 0;
 
 
 	if(mA->height*mA->width != mB->width*mB->height){
@@ -40,17 +34,19 @@ int matrix_matrix_mult(Matrix *mA, Matrix *mB, Matrix *mC){
 	float *bufferC = mC->rows;
 	
 	__m256 vec_c = _mm256_setzero_ps();
-	for(i=0; i<mA->width*mA->height; i++){
-		if(i%mA->width == 0)
+	for(int i=0 ; i < mA->width*mA->height ; i++)
+	{
+		if(i % mA->width == 0)
 			bufferB = mB->rows;
 
 		__m256 vec_aij = _mm256_set1_ps(bufferA[i]);
+		
 		bufferC = mC->rows + mB->width * (i/mA->width);
 
-		for(j=0; j<mB->width; j+=8, bufferB+=8,bufferC+=8){
+		for(int j=0; j<mB->width; j+=8, bufferB+=8,bufferC+=8){
 			__m256 vec_b = _mm256_load_ps(bufferB);
 			__m256 vec_c = _mm256_load_ps(bufferC);
-			__m256 result = _mm256_fmadd_ps(vec_aij,vec_b,vec_c);
+			__m256 result = _mm256_fmadd_ps(vec_aij, vec_b,vec_c);
 			_mm256_store_ps(bufferC, result);
 		}
 	}
--- trab1/src/matrix_lib_test.c
+++ trab1/src/matrix_lib_test.c
@@ -2,12 +2,12 @@
 
 int main(int argc, char **argv){
 	float scalar = atof(argv[1]);
-	int count;
-	Matrix matA, matB;
-	matA.height = atoi(argv[2]);
-	matA.width = atoi(argv[3]);
-	matB.height = atoi(argv[4]);
-	matB.width =atoi(argv[5]);
+	int count = 0;
+	Matrix mA, mB;
+	mA.height = atoi(argv[2]);
+	mA.width = atoi(argv[3]);
+	mB.height = atoi(argv[4]);
+	mB.width =atoi(argv[5]);
 	int mat_scalar, mat_mult;
 
 
@@ -19,63 +19,64 @@ int main(int argc, char **argv){
 
 	if(file1 == NULL || file2 ==NULL)
 	{
-		fprintf(stdout, ".dat failed to open.  exiting...\n");
-		exit(1);
+		printf("could not open one or more .dat files\n");
+		return 0;
 	}
 
-	count=0;
-	
-	float* vetA= (float*)aligned_alloc(32, (matA.height*matA.width) * sizeof(float));
-	float vetA_aux;
-
-	for(int i=0; i<matA.height*matA.width; i++){
-		fread((void*)(&vetA_aux), sizeof(vetA_aux), 1, file1);
-		vetA[count]=vetA_aux;
+	float *vA= (float*)aligned_alloc(32, (mA.height*mA.width) * sizeof(float));
+	float aux;
+	for(int i=0; i < mA.height * mA.width; i++)
+	{
+		fread((void*)(&aux), sizeof(aux), 1, file1);
+		vA[count]=aux;
 		count++;
 	}
-	matA.rows = vetA;
+	mA.rows = vA;
 	
 	count=0;
-	float* vetB= (float*)aligned_alloc(32, (matB.height*matB.width) * sizeof(float));
-	float vetB_aux;
-	
-	for(int i=0; i<matB.height*matB.width; i++){
-		fread((void*)(&vetB_aux), sizeof(vetB_aux), 1, file2);
-		vetB[count]=vetB_aux;
+	float* vB= (float*)aligned_alloc(32, (mB.height*mB.width) * sizeof(float));
+	for(int i=0; i < mB.height * mB.width; i++)
+	{
+		fread((void*)(&aux), sizeof(aux), 1, file2);
+		vB[count]=aux;
 		count++;
 	}
-	matB.rows = vetB;
+	mB.rows = vB;
 	
-	Matrix matC;
-	matC.height = matA.height;
-	matC.width = matB.width;
+	Matrix mC;
+	mC.height = mA.height;
+	mC.width = mA.width;
 
-	float* vetC= (float*)aligned_alloc(32, (matC.height*matC.width) * sizeof(float));
+	float* vC= (float*)aligned_alloc(32, (mC.height*mC.width) * sizeof(float));
 	
-	for(int i=0; i< (matC.height*matC.width); i++){
-		vetC[i] = 0;
-	}
-	matC.rows = vetC;
+	for(int i=0 ; i < (mC.height*mC.width); i++)
+		vC[i] = 0;
 	
-	mat_scalar = scalar_matrix_mult(scalar, &matA);
+	mC.rows = vC;
+	
+	mat_scalar = scalar_matrix_mult(scalar, &mA);
 	if(!mat_scalar)
+	{
 		printf("Error while multiplying by scalar\n");
-
-	for(int i=0; i<matA.height*matA.width; i++){	
- 		fwrite((void*)(&matA.rows[i]), sizeof(matA.rows[i]), 1, result1);
+		return 0;
 	}
+
+	for(int i=0 ; i < mA.height * mA.width ; i++)	
+ 		fwrite((void*)(&mA.rows[i]), sizeof(mA.rows[i]), 1, result1);
 	
-	mat_mult = matrix_matrix_mult(&matA, &matB, &matC);
+	mat_mult = matrix_matrix_mult(&mA, &mB, &mC);
 	if(!mat_mult)
+	{
 		printf("Error while multiplying matrix A by matrix B");
-
-
-	for(int i=0; i<matC.height*matC.width; i++){	
- 		fwrite((void*)(&matC.rows[i]), sizeof(matC.rows[i]), 1, result2);
+		return 0;
 	}
-	free(vetA);
-	free(vetB);
-	free(vetC);
+
+	for(int i=0; i<mC.height*mC.width; i++)
+ 		fwrite( (void*)( &mC.rows[i] ), sizeof( mC.rows[i] ), 1, result2 );
+	
+	free(vA);
+	free(vB);
+	free(vC);
 	fclose(file1);
 	fclose(file2);
 	fclose(result1);
